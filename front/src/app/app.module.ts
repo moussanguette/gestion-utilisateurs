7b7db1847e21d4e1338600b26d69d8060e957d30
vivishu <kadiegui1999@gmail.com>
2021-04-26 16:12:47 +0000

    creation de composants admin et etudiant

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './composants/admin/admin.component';
+import { EtudiantComponent } from './composants/etudiant/etudiant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    EtudiantComponent
   ],
   imports: [
     BrowserModule,
