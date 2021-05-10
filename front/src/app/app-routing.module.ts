f850f64f5b3dbcceef4602fe40056914e2b76283
assane <assane.thiaw9@gmail.com>
2021-05-10 11:11:33 +0100

    google

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
 import { ConnexionComponent } from './composants/connexion/connexion.component';
+import { InscriptionComponent } from './composants/inscription/inscription.component';
 import { MotdepasseoublierComponent } from './composants/motdepasseoublier/motdepasseoublier.component';
 
 const routes: Routes = [
-  {path:'connexion',component:ConnexionComponent},
-  {path:'oublier',component:MotdepasseoublierComponent}
+  {path:'connexion',
+  component: ConnexionComponent
+},
+  {path:'oublier',
+  component:MotdepasseoublierComponent
+},
+{ path: 'inscription',
+ component:InscriptionComponent
+}
 ];
 
 @NgModule({
--- front/src/app/composants/connexion/connexion.component.ts
+++ front/src/app/composants/connexion/connexion.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, NgZone, OnInit, Renderer2 } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-connexion',
@@ -7,11 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnexionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private metaservice: Meta,
+    @Inject(DOCUMENT) private doc : Document,
+    private renderer: Renderer2,
+    ngZone: NgZone
+  ) {
+    window['onSignIn'] = User => ngZone.run(
+      ()  => {
+        this.afterSignUp(User);
+      }
+
+    );
+   }
 
   ngOnInit(): void {
+    this.metaservice.addTags([
+      {name: 'google-signin-client_id', content :'698363782994-6rnm5babk2fee5vn5c0s9utb9f14142i.apps.googleusercontent.com'}
+    ]);
+
+    let script = this.renderer.createElement('script');
+    script.scr = 'https://apis.google.com/js/platform.js';
+    script.defer = true;
+    script.async = true;
+    this.renderer.appendChild(document.body, script);
   }
 
+  afterSignUp(googleUser) {
+    console.log(googleUser);
+    //this.guser = googleUser;
+  }
+  
   connecter(f){
     console.log(f.value);
   }
