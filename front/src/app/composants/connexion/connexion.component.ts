d479460b9c8225fd1fc0f18fa9b84d624985fcaa
simplon <niangmohamet4@gmail.com>
2021-05-25 17:48:50 +0000

    connexion google ok

--- front/src/app/composants/connexion/connexion.component.ts
+++ front/src/app/composants/connexion/connexion.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, NgZone, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConnexionService } from '../../services/connexion.service';
 import { NgForm } from '@angular/forms'
+
+import { Meta } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-connexion',
@@ -14,13 +17,46 @@ status:any
 data:any
 id :any
 validation : any
-  constructor(private connexionService:ConnexionService, private route:Router) { }
+  constructor(private connexionService:ConnexionService, private route:Router, private metaservice: Meta,
+    @Inject(DOCUMENT) private doc : Document,
+    private renderer: Renderer2,
+    ngZone: NgZone
+  ) {
+    window['onSignIn'] = User => ngZone.run(
+      ()  => {
+        this.afterSignUp(User);
+      }
+
+    );
+   }
 
   ngOnInit(): void {
+    this.metaservice.addTags([
+      {name: 'google-signin-client_id', content :'698363782994-6rnm5babk2fee5vn5c0s9utb9f14142i.apps.googleusercontent.com'}
+    ]);
+
+    let script = this.renderer.createElement('script');
+    script.scr = 'https://apis.google.com/js/platform.js';
+    script.defer = true;
+    script.async = true;
+    this.renderer.appendChild(document.body, script);
   }
+
+  afterSignUp(googleUser) {
+    console.log(googleUser);
+    //this.guser = googleUser;
+  }
+
+  /* connecter(f){
+    console.log(f.value);
+  } */
+
+
+  
   hide = true;
   roleUser : any
   connecter(f:NgForm){
+    
     this.form = f.value
     this.validation=f.status
     return this.connexionService.donne(this.form).subscribe((resultat:any)=>{
